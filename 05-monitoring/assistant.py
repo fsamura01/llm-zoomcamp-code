b36ad2c842593695dc1358aa5ed6d292ee51847e
fsamura01 <multicloudstudent01@gmail.com>
2026-07-20 14:59:13 +0000

    feat(database): Initialize database schema and add conversation logging
    - Created db_init.py to set up the database connection and initialize the llm_call_records table.
    - Added db_query.py for querying conversation records from the database.
    - Implemented db_save.py to save conversation records into the database.
    - Introduced db_query.ipynb for interactive querying of conversation records.
    - Updated pyproject.toml and uv.lock to include psycopg for PostgreSQL database interaction.

--- 05-monitoring/assistant.py
+++ 05-monitoring/assistant.py
@@ -8,6 +8,8 @@ from ingest import load_faq_data, build_index
 # from rag_helper import RAGBase
 from metrics import RAGWithMetrics
 
+from db_save import save_conversation
+
 
 def create_assistant():
     load_dotenv()
@@ -37,3 +39,5 @@ if __name__ == "__main__":
         
      answer = assistant.rag(query)
      print(answer)
+
+     save_conversation(assistant.last_call, query, "llm-zoomcamp")
